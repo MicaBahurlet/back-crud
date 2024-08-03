c5b0bef1f310282dbf969fe1f4f1fbf9676d9e8e
Micaela Bahurlet <micabahurletgo@gmail.com>
2024-08-03 14:38:37 -0300

    change verceljson and api folder

--- dist/helpers/constants.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.ROLES = void 0;
-//acá voy a definir los roles de las personas que se registren en la base de datos. 
-exports.ROLES = {
-    admin: "adminRole",
-    user: "userRole",
-};
